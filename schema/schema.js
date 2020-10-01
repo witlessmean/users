493ba5d6294de1c5e7fb21184e9758187ef74bf8
Shawn Elston <elstonshawn1@yahoo.com>
2020-10-01 08:09:12 -0700

    learning graphQL

---
+++ schema/schema.js
@@ -0,0 +1,52 @@
+const graphql = require('graphql');
+const axios = require('axios');
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema
+} = graphql;
+
+
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString }
+    }
+});
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        company: {
+            type: CompanyType
+        }
+    }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args) {
+               return axios.get(`http://localhost:3000/users/${args.id}`)
+                .then((res) => {
+                   return res.data
+                })
+            }
+        }
+    }
+});
+
+const ourSchema = new GraphQLSchema({
+    query: RootQuery
+});
+
+module.exports = ourSchema
